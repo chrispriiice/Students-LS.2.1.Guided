bb1af119ef2c78e33d81139805448a0fa3626e3c
Chris Price <christopher.r.price@gmail.com>
2020-03-10 21:49:08 -0400

    Initial commit.

---
+++ Students/Model/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  Students
+//
+//  Created by Andrew R Madsen on 8/5/18.
+//  Copyright © 2018 Lambda Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Student: Codable {
+    var name: String
+    var course: String
+}
